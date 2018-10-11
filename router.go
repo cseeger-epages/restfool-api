81eeec3fd49d8ccf29e7637674050d0426e34e14
Carsten Seeger <c.seeger@epages.com>
2018-10-11 12:06:00 +0200

    replaced throttled.v2 with dipdip/tollbooth

--- router.go
+++ router.go
@@ -1,36 +1,11 @@
-/*
-   Restfool-go
-
-   Copyright (C) 2018 Carsten Seeger
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-   @author Carsten Seeger
-   @copyright Copyright (C) 2018 Carsten Seeger
-   @license http://www.gnu.org/licenses/gpl-3.0 GNU General Public License 3
-   @link https://github.com/cseeger-epages/rest-api-go-skeleton
-*/
-
 package restfool
 
 import (
 	"net/http"
 
+	"github.com/didip/tollbooth"
 	"goji.io"
 	"goji.io/pat"
-	throttled "gopkg.in/throttled/throttled.v2"
-	"gopkg.in/throttled/throttled.v2/store/memstore"
 )
 
 // NewRouter is the router constructor
@@ -47,26 +22,21 @@ func (a RestAPI) NewRouter() *goji.Mux {
 
 // AddRoutes add default handler, routing and ratelimit
 func (a RestAPI) AddRoutes(router *goji.Mux) {
-	store, err := memstore.New(65536)
-	Error("ROUTES: could not create memstore", err)
 
-	// rate limiter
-	quota := throttled.RateQuota{
-		MaxRate:  throttled.PerMin(a.Conf.RateLimit.Limit),
-		MaxBurst: a.Conf.RateLimit.Burst,
-	}
-	rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
-	Error("ROUTES: error in ratelimiting", err)
-
-	httpRateLimiter := throttled.HTTPRateLimiter{
-		RateLimiter: rateLimiter,
-		VaryBy:      &throttled.VaryBy{Path: true},
-	}
 	for _, route := range routes {
 		var handler http.Handler
 
 		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
+		handler = Logger(
+			tollbooth.LimitHandler(
+				tollbooth.NewLimiter(
+					float64(a.Conf.RateLimit.Limit),
+					nil,
+				),
+				handler,
+			),
+			route.Name,
+		)
 
 		var pattern *pat.Pattern
 
@@ -93,7 +63,8 @@ func (a RestAPI) AddRoutes(router *goji.Mux) {
 
 		router.Handle(
 			pattern,
-			use(handler, a.addDefaultHeader, a.basicAuthHandler, httpRateLimiter.RateLimit),
+			//use(handler, a.addDefaultHeader, a.basicAuthHandler, httpRateLimiter.RateLimit),
+			use(handler, a.addDefaultHeader, a.basicAuthHandler),
 		)
 
 		/*
